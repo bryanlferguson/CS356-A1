b9ad5ecaffb942cd45b6f287ec81161b08dd05d6
fergo <fergo@10.110.196.16>
2018-07-03 11:13:50 -0700

    Added comments for methods. Removed unused method.

--- cs356/src/assignment1/IVoteService.java
+++ cs356/src/assignment1/IVoteService.java
@@ -8,11 +8,18 @@ import java.util.Map.Entry;
 public class IVoteService {
 	private Question q;
 	private List<Student> students;
-
+	
+	/*
+	 * Constructor for IVoteService. Initializes the list of students.
+	 */
 	public IVoteService() {
 		students = new ArrayList<Student>();
 	}
 	
+	/*
+	 * Sets the question string, as well as if the question will take multiple answers.
+	 * Creates a new Question object accordingly.
+	 */
 	public void setQuestion(String ques, boolean multipleAnswers) {
 		if (multipleAnswers) {
 			q = new MultipleAnswerQuestion(ques);
@@ -20,22 +27,42 @@ public class IVoteService {
 			q = new SingleAnswerQuestion(ques);
 	}
 	
+	/*
+	 * Standard getter for question.
+	 */
 	public Question getQuestion() {
 		return q;
 	}
 	
+	/*
+	 * Standard setter for student.
+	 */
 	public void addStudent(Student s) {
 		students.add(s);
 	}
+	
+	/*
+	 * Adds a new student to the list. Generates with a random ID and returns
+	 * the new Student object.
+	 */
 	public Student addRandomStudent() {
 		Student stu = new Student(Integer.toString((int) (Math.random() * 100000000)));
 		students.add(stu);
 		return stu;
 	}
+	
+	/*
+	 * Standard getter for students list.
+	 */
 	public List<Student> getStudents(){
 		return students;
 	}
 	
+	/*
+	 * Compiles results from the question votes and prints the number of correct responses
+	 * and incorrect responses. Uses the getResults() method from the Question class to 
+	 * do this.
+	 */
 	public void printResults() {
 		Map<String, Integer> res = q.getResults();
 		int correct = 0;
@@ -52,17 +79,18 @@ public class IVoteService {
 		System.out.println("Number incorrect: " + (total - correct));
 	}
 	
-	public void printCorrect() {
-		Map<String, Integer> res = q.getResults();
-		for (Entry<String, Integer> pair: res.entrySet()) {
-			System.out.println(pair.getKey().toString() + "\t" + pair.getValue());
-		}
-	}
-	
+	/*
+	 * Casts a vote for a student given a student and an answer. This is mostly 
+	 * just a wrapper for setAnswers in Question.
+	 */
 	public void vote(Student s, Answer a) {
 		q.setAnswers(s, a);
 	}
 	
+	/*
+	 * Default for creating a question accepting multiple answers with answer choices A,B,C,D
+	 * Each answer choice has 50% chance of being a correct answer
+	 */
 	public void setABCDMultiple() {
 		q.addAnswerChoice(new Answer("A"), Math.random() < .5);
 		q.addAnswerChoice(new Answer("B"), Math.random() < .5);
@@ -70,7 +98,10 @@ public class IVoteService {
 		q.addAnswerChoice(new Answer("D"), Math.random() < .5);
 		
 	}
-	
+	/*
+	 * Default for creating a question accepting single answer with answer choices A,B,C,D
+	 * Each answer choice has an equal chance of being the correct one.
+	 */
 	public void setABCDSingle() {
 		int c = (int) (Math.random() * 4);
 		q.addAnswerChoice(new Answer("A"), c == 0);
