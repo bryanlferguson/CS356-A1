e869be73efa33cbaa893b3069c564dc523008678
fergo <fergo@10.110.196.16>
2018-07-03 11:32:18 -0700

    Comments.

--- cs356/src/assignment1/SingleAnswerQuestion.java
+++ cs356/src/assignment1/SingleAnswerQuestion.java
@@ -10,32 +10,55 @@ public class SingleAnswerQuestion extends Question{
 	private Map<String, Answer> answers;
 	private Answer correct;
 	
+	/*
+	 * Constructor for single answer question. Initializes the question string using super
+	 * and initializes the HashMap to store student answers. Since each student can only answer
+	 * with one answer, it does not need to be a list. Additionally, the correct answer does not
+	 * need to be a list either.
+	 */
 	public SingleAnswerQuestion(String q) {
 		super(q);
 		answers = new HashMap<String, Answer>();
 	}
-
+	
+	/*
+	 * Sets a student's vote given a student and an answer.
+	 */
 	public void setAnswers(Student s, Answer a) {
 		answers.put(s.getID(), a);
 	}
-
+	
+	/*
+	 * Changes the correct answer.
+	 */
 	public void addCorrectAnswer(Answer a) {
 		correct = a;
 	}
-
+	
+	/*
+	 * Adds an answer choice to the correct answers list. The boolean value 
+	 * determines whether or not is is a correct answer.
+	 */
 	public void addAnswerChoice(Answer a, boolean c) {
 		if (c)
 			correct = a;
 		addAnswerChoice(a);
 	}
-
 	
+	/*
+	 * Returns the correct answers for this question. The list will always have a 
+	 * length of one.
+	 */
 	public ArrayList<Answer> getCorrect() {
 		ArrayList<Answer> ret = new ArrayList<Answer>();
 		ret.add(correct);
 		return ret;
 	}
-
+	
+	/*
+	 * Tallies the results from the vote and returns it as a Map with key string and 
+	 * value integer. 
+	 */
 	public Map<String, Integer> getResults() {
 		Map<String, Integer> ret = new HashMap<String, Integer>();
 		for (Answer value: answers.values()) {
