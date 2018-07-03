0f903cac4d48488b55f99a492c9960f97d6b8780
fergo <fergo@10.110.196.16>
2018-07-03 11:27:39 -0700

    Added description of simulation carried out.

--- cs356/src/assignment1/SimulationDriver.java
+++ cs356/src/assignment1/SimulationDriver.java
@@ -3,8 +3,19 @@ package assignment1;
 import java.util.ArrayList;
 
 public class SimulationDriver {
-
+	
+	/**
+	 * This section of code is used to simulate question creation and student voting.
+	 * The question has a 50% chance of being multiple answer, or single answer. 
+	 * The amount of students is random from 0 to 100.
+	 * Answer choices are fixed at A,B,C,D. Correct answer(s) are random.
+	 * Students will vote randomly. 
+	 * When the program is run, it will display whether or not it was a single or multiple
+	 * answer, what the correct answers are, the results of the votes, and the number
+	 * correct and incorrect answers. Rerun to see different results.
+	 */
 	public static void main(String[] args) {
+		
 		IVoteService IVS = new IVoteService();
 		
 		boolean multipleAnswers = Math.random() < .5;
@@ -12,9 +23,6 @@ public class SimulationDriver {
 		System.out.println(multipleAnswers ? "Multiple answers" : "Single answer");
 		IVS.setQuestion("Template question?", multipleAnswers);
 	
-		
-		
-		
 		if (multipleAnswers) {
 			IVS.setABCDMultiple();
 			String[] randAnswers = { "A", "B", "C", "D" };
