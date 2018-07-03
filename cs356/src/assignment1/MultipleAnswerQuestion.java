4251bc74272d8a0d1d1d2a69ce1d89c1f68d1872
fergo <fergo@10.110.196.16>
2018-07-03 11:24:57 -0700

    Added some comments to describe methods.

--- cs356/src/assignment1/MultipleAnswerQuestion.java
+++ cs356/src/assignment1/MultipleAnswerQuestion.java
@@ -10,16 +10,29 @@ public class MultipleAnswerQuestion extends Question{
 	private Map<String, ArrayList<Answer>> answers;
 	private List<Answer> correct;
 
+	/*
+	 * Constructor for a question that has multiple answers. This class contains a 
+	 * map to store votes so that no student can answer more than once. The key is 
+	 * the student ID, and the value is an ArrayList of their choices made. All of these
+	 * are initialized in the constructor.
+	 */
 	public MultipleAnswerQuestion(String q) {
 		super(q);
 		answers = new HashMap<String, ArrayList<Answer>>();
 		correct = new ArrayList<Answer>();
 	}
 
+	/*
+	 * Casts a student's vote. Overrides the values if they have already voted.
+	 */
 	public void setAnswers(Student s, ArrayList<Answer> a) {
 		answers.put(s.getID(), a);
 	}
 
+	/*
+	 * Adds a students vote to their currently selected answers. Will not override their 
+	 * previous choices, just add to them.
+	 */
 	public void setAnswers(Student s, Answer a) {
 		ArrayList<Answer> temp;
 		if (answers.get(s.getID()) == null)
@@ -29,17 +42,29 @@ public class MultipleAnswerQuestion extends Question{
 		temp.add(a);
 		answers.put(s.getID(), temp);
 	}
-
+	
+	/*
+	 * Adds an answer as to the correct answer list.
+	 */
 	public void addCorrectAnswer(Answer a) {
 		correct.add(a);
 	}
 	
+	/*
+	 * Adds an answer choice to the question. Adds to the answer list. Boolean parameter
+	 * determines whether or not to make it a correct answer or not.
+	 */
 	public void addAnswerChoice(Answer a, boolean c) {
 		if (c)
 			correct.add(a);
 		addAnswerChoice(a);
 	}
-
+	
+	/*
+	 * Iterates through the map of votes and tallies up the number of responses
+	 * per answer choice. Returns the results in a new Map using String key and Integer 
+	 * Value.
+	 */
 	public Map<String, Integer> getResults() {
 		Map<String, Integer> ret = new HashMap<String, Integer>();
 		for (ArrayList<Answer> al: answers.values()) {
@@ -51,7 +76,10 @@ public class MultipleAnswerQuestion extends Question{
 		}
 		return ret;
 	}
-
+	
+	/*
+	 * Getter method for the correct answers list.
+	 */
 	public ArrayList<Answer> getCorrect() {
 		return (ArrayList<Answer>) correct;
 	}
